deedbe37582d36ad25d78be6b5b666ac31631074
HOUANLOKONON Don-juste <houanlokonondekajuste@gmail.com>
2026-08-11 12:26:26 +0200

    changement

--- database.py
+++ database.py
@@ -4,7 +4,10 @@ from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
 load_dotenv()
 import os
-DATABASE_URL=os.getenv("DATABASE_URL")
-engine=create_engine(DATABASE_URL,connect_args={"check_same_thread":False})
-SessionLocal=sessionmaker(bind=engine)
+DATABASE_URL=os.getenv("DATABASE_URL","sqlite:///./ma_base.db")
+if DATABASE_URL.startswith("postgres://"):
+    DATABASE_URL=DATABASE_URL.replace("postgres://","postgresqlite://",1)
+engine=create_engine(DATABASE_URL)
 Base=declarative_base()
+SessionLocal=sessionmaker(autocommit=False,autoflush=False,bind=engine)
+
