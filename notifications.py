37698b7348c1d9412b5ec35a0a8781c7743c1840
HOUANLOKONON Don-juste <houanlokonondekajuste@gmail.com>
2026-08-14 20:17:42 +0200

    modifier varaibles

--- notifications.py
+++ notifications.py
@@ -1,21 +1,20 @@
+import os
+import json
 import firebase_admin
 from firebase_admin import credentials, messaging
 
-cred = credentials.Certificate("firebase-key.json")
+firebase_credentials_json = os.getenv("FIREBASE_CREDENTIALS")
+firebase_credentials_dict = json.loads(firebase_credentials_json)
+cred = credentials.Certificate(firebase_credentials_dict)
 firebase_admin.initialize_app(cred)
 
 def envoyer_notification(fcm_token: str, titre: str, corps: str):
     if fcm_token is None:
         return
-    
     message = messaging.Message(
-        notification=messaging.Notification(
-            title=titre,
-            body=corps
-        ),
+        notification=messaging.Notification(title=titre, body=corps),
         token=fcm_token
     )
-    
     try:
         messaging.send(message)
     except Exception as e:
